c508e3049eed2cd0d6499ccaf05e1dcaf7f53b29
Ethan Borg <eborg4@uwo.ca>
2025-05-16 14:36:17 -0400

    Main Page done for now

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import styles from "./main.module.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -20,6 +21,87 @@ export default function Home() {
     document.body.removeChild(link);
   };
 
+  const [activeTab, setActiveTab] = useState("work");
+
+  const workContent = (
+    <div className={styles.experienceContent}>
+      <div>
+        <ul className={styles.row}>
+          <li className={styles.rowContent}>
+            <span className={styles.imageContainer}>
+              <img className={styles.rowImage} src="Images/Purchs-Image.jpg" />
+            </span>
+            <div className={styles.rowDetails}>
+              <time>Jul 2024 - Present</time>
+              <h2>Purchs</h2>
+              <p>Software Engineer</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className={styles.row}>
+          <li className={styles.rowContent}>
+            <span className={styles.imageContainer}>
+              <img
+                className={styles.rowImage}
+                src="Images/ivey_business_school_logo.jpg"
+              />
+            </span>
+            <div className={styles.rowDetails}>
+              <time>Sep 2023 - Apr 2024</time>
+              <h2>Ivey Business School at Western University</h2>
+              <p>Research Assistant</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+
+  const educationContent = (
+    <div className={styles.experienceContent}>
+      <div>
+        <ul className={styles.row}>
+          <li className={styles.rowContent}>
+            <span className={styles.imageContainer}>
+              <img
+                className={styles.rowImage}
+                src="Images/westernuniversity_logo.jpg"
+              />
+            </span>
+            <div className={styles.rowDetails}>
+              <time>Sep 2020 - Apr 2024</time>
+              <h2>University of Western Ontario</h2>
+              <p>
+                Bachelor of Science (BSc), Honours Specialization In Computer
+                Science
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+
+  const skills = [
+    "JavaScript",
+    "React",
+    "Next.JS",
+    "HTML",
+    "CSS",
+    "Java",
+    "Python",
+    "MongoDB",
+    "GCP",
+    "Figma",
+    "Agile",
+    "Jira",
+    "PostgreSQL",
+    "Git",
+    "REST API's",
+  ];
+
   return (
     <div className={styles.root}>
       <div className={styles.wrapper}>
@@ -33,7 +115,7 @@ export default function Home() {
                 Computer Geek
               </h3>
               <div className={styles.location}>
-                <FaMapMarkerAlt />
+                <FaMapMarkerAlt size={20} />
                 <p>Ontario, Canada</p>
               </div>
               <div className={styles.links}>
@@ -55,6 +137,39 @@ export default function Home() {
                 >
                   <FaGithub size={28} />
                 </a>
+              </div>
+            </div>
+            <div className={styles.experience}>
+              <div>
+                <div className={styles.experienceTabs}>
+                  <button
+                    className={activeTab === "work" ? styles.activeTab : ""}
+                    onClick={() => setActiveTab("work")}
+                  >
+                    Work
+                  </button>
+                  <button
+                    className={
+                      activeTab === "education" ? styles.activeTab : ""
+                    }
+                    onClick={() => setActiveTab("education")}
+                  >
+                    Education
+                  </button>
+                </div>
+                <div>
+                  {activeTab === "work" ? workContent : educationContent}
+                </div>
+              </div>
+            </div>
+            <div className={styles.skills}>
+              <span>Skills</span>
+              <div className={styles.skillsList}>
+                {skills.map((skill, index) => (
+                  <div key={index} className={styles.eachSkill}>
+                    {skill}
+                  </div>
+                ))}
               </div>
             </div>
             <Footer />
